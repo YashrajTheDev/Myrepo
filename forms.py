b0c15b403d9988b0dc8ea4e53fca609913720987
YashrajTheDev <128779095+YashrajTheDev@users.noreply.github.com>
2024-10-24 01:42:59 +0530

    Create forms.py

---
+++ forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class CustomerRegistrationForm(FlaskForm):
+    name = StringField('Customer Name', validators=[DataRequired()])
+    email = StringField('Customer Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Register Customer')
